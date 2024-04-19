73ae0b89befdb711544f4639ba5f68f954185f01
vladislavDunin <vladislavdunin.java@gmail.com>
2024-04-19 21:29:03 +0300

    Add ReverseOnlyVowelsInString class

---
+++ src/main/java/com/mydailyroutine/algorithms/ReverseOnlyVowelsInString.java
@@ -0,0 +1,50 @@
+package com.mydailyroutine.algorithms;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ReverseOnlyVowelsInString implements AlgosActionDispatcher {
+    private String text = "Java execute";
+    @Override
+    public void execute() {
+       String result = reverseVowels(text);
+
+       System.out.println(result);
+    }
+
+
+    public String reverseVowels(String s) {
+        List<String> vowels = new ArrayList<>();
+        Collections.addAll(vowels, "a", "e", "u", "i", "o");
+        String[] sArray = s.split("");
+
+
+        int head = 0;
+        int tail = sArray.length - 1;
+
+        while (head <= tail) {
+            if (vowels.contains(sArray[head].toLowerCase()) && vowels.contains(sArray[tail].toLowerCase())) {
+                String tmp = sArray[head];
+                sArray[head] = sArray[tail];
+                sArray[tail] = tmp;
+
+                head++;
+
+                if (tail > 0) {
+                    tail--;
+                }
+            }
+
+            if (head < tail && !vowels.contains(sArray[head].toLowerCase())) {
+                head++;
+            }
+            if (!vowels.contains(sArray[tail].toLowerCase())) {
+                tail--;
+            }
+
+        }
+
+        return String.join("", sArray);
+    }
+}
