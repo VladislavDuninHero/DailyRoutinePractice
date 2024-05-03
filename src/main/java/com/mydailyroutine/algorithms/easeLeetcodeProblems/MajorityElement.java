97010bf371e5b1203682c84ba5d7927bf1801ebb
vladislavDunin <vladislavdunin.java@gmail.com>
2024-05-03 19:48:39 +0300

    Add findMajorityElement class

---
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/MajorityElement.java
@@ -0,0 +1,44 @@
+package com.mydailyroutine.algorithms.easeLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MajorityElement implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+        int[] nums = {8, 8, 7, 7, 7};
+
+        int result = findMajorityElement(nums);
+        System.out.println(result);
+    }
+
+    private int findMajorityElement(int[] nums) {
+
+        Map<Integer, Integer> statisticOfElements = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (statisticOfElements.containsKey(nums[i])) {
+                statisticOfElements.put(nums[i], statisticOfElements.get(nums[i]) + 1);
+                continue;
+            }
+
+            statisticOfElements.put(nums[i], 1);
+        }
+
+        int max = 1;
+        int majorityNum = nums[0];
+
+        for (Map.Entry<Integer, Integer> data : statisticOfElements.entrySet()) {
+            if (data.getValue() > max) {
+                max = data.getValue();
+                majorityNum = data.getKey();
+            }
+
+        }
+
+        return majorityNum;
+    }
+}
