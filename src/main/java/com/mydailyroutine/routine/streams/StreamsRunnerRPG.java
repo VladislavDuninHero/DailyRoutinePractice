a2ecb1b82aa5e1ea84f9b467dffbe78acce73a85
vladislavDunin <vladislavdunin.java@gmail.com>
2024-05-12 00:50:07 +0300

    Add 2, 3, 4 tasks in streams package

--- src/main/java/com/mydailyroutine/routine/streams/StreamsRunnerRPG.java
+++ src/main/java/com/mydailyroutine/routine/streams/StreamsRunnerRPG.java
@@ -1,9 +1,12 @@
 package com.mydailyroutine.routine.streams;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamsRunnerRPG {
     //    1. найти множество всех рас
@@ -19,10 +22,37 @@ public class StreamsRunnerRPG {
                 .stream()
                 .flatMap((el) -> el.getValue()
                         .stream())
-                .map((el) -> el.getRace())
+                .map(GameObject::getRace)
                 .collect(Collectors.toSet());
         System.out.println("1. найти множество всех рас: " + races);
 
+        BigDecimal charactersGold = BigDecimal.valueOf(
+                world.entrySet().stream()
+                        .flatMap((el) -> el.getValue().stream())
+                        .mapToInt(GameObject::getGold)
+                        .sum()
+        );
+
+        System.out.println("2. посчитать общее количество золота на карте: " + charactersGold);
+
+        long countOfXCoordinate = world.entrySet().stream()
+                .map((el) -> el.getKey())
+                .filter((el) -> el.getX() == 2)
+                .count();
+        System.out.println("3. найти количество объектов по координате x = 2: " + countOfXCoordinate);
+
+        List<GameObject> thirdPlaceForGold = world.entrySet().stream()
+                .flatMap((el) -> el.getValue().stream())
+                .sorted((a, b) -> b.getGold() - a.getGold())
+                .limit(3)
+                .min((a, b) -> a.getGold() - b.getGold())
+                        .stream().toList();
+
+        System.out.println(
+                "4. найти третьего по количеству золота на карте: "
+                        + thirdPlaceForGold.get(0).getRace() + ", "
+                        + thirdPlaceForGold.get(0).getGold()
+        );
 
     }
 }
