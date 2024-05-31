d7c7eca0d994bf2f567b2fc56cee3a92e9ae63b3
vladislavDunin <vladislavdunin.java@gmail.com>
2024-05-31 23:23:02 +0300

    Add Fibonacci class

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/Fibonacci.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/Fibonacci.java
@@ -7,6 +7,7 @@ public class Fibonacci implements AlgosActionDispatcher {
     @Override
     public void execute() {
         System.out.println(fib(4));
+        System.out.println(fibIteration(4));
     }
 
     //recursion example
@@ -16,6 +17,24 @@ public class Fibonacci implements AlgosActionDispatcher {
         }
 
         return fib(n - 1) + fib(n - 2);
+    }
+
+    private int fibIteration(int n) {
+        int prev = 0;
+        int next = 1;
+        int result = 0;
+
+        if (n <= 1) {
+            return n;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            result = prev + next;
+            prev = next;
+            next = result;
+        }
+
+        return result;
     }
 
 }
