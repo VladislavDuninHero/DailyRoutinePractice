efec11e94147896a20cab6961f0693a94c7c70b2
vladislavDunin <vladislavdunin.java@gmail.com>
2024-06-07 21:15:31 +0300

    Fixed lastStoneWeight method in LastStoneWeight class

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/LastStoneWeight.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/LastStoneWeight.java
@@ -2,9 +2,7 @@ package com.mydailyroutine.algorithms.easeLeetcodeProblems;
 
 import com.mydailyroutine.algorithms.AlgosActionDispatcher;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 public class LastStoneWeight implements AlgosActionDispatcher {
 
@@ -16,42 +14,34 @@ public class LastStoneWeight implements AlgosActionDispatcher {
     }
 
     private int lastStoneWeight(int[] stones) {
-        int[] pair = {0, 0};
-        List<Integer> newStones = new ArrayList<>();
-
-        Arrays.sort(stones);
-
-        int head = 0;
-        int tail = stones.length - 1;
-
-        while (head < stones.length - 1) {
-            if (tail < 0) {
-                head++;
-                tail = stones.length - 1;
-            }
-
-            if (tail > 2) {
-                pair[0] = stones[tail];
-                pair[1] = stones[tail - 1];
-            }
-
-
-            if (pair[0] != pair[1]) {
-                newStones.add(pair[0] - pair[1]);
-            }
-
-
-
-
-            System.out.println(Arrays.toString(pair));
-            System.out.println(Arrays.toString(stones));
-
-            tail -= 2;
+        if (stones.length == 1) {
+            return stones[0];
         }
 
+        int lastWeight = 0;
 
-        System.out.println(newStones);
 
-        return 0;
+        for (int i = stones.length - 1; i > 0; i-- ) {
+            Arrays.sort(stones);
+
+            if (stones[stones.length - 1] == stones[stones.length - 2]) {
+                stones[stones.length - 1] = 0;
+                stones[stones.length - 2] = 0;
+                continue;
+            }
+
+            int temp = stones[stones.length - 1] - stones[stones.length - 2];
+            stones[stones.length - 1] = 0;
+            stones[stones.length - 2] = temp;
+
+        }
+
+        for (int i = 0; i < stones.length; i++) {
+            if (stones[i] != 0) {
+                lastWeight = stones[i];
+            }
+        }
+
+        return lastWeight;
     }
 }
