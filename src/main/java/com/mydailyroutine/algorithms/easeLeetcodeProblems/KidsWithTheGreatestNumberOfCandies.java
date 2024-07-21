ca9248e163fbcfa4e0f25874f4b694300729deb7
vladislavDunin <vladislavdunin.java@gmail.com>
2024-07-21 12:57:30 +0300

    Refactor KidsWithTheGreatestNumberOfCandies class

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/KidsWithTheGreatestNumberOfCandies.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/KidsWithTheGreatestNumberOfCandies.java
@@ -6,6 +6,26 @@ import java.util.*;
 
 public class KidsWithTheGreatestNumberOfCandies implements AlgosActionDispatcher {
 
+    //Description
+    //There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+    //
+    //Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+    //
+    //Note that multiple kids can have the greatest number of candies.
+    //
+    //
+    //
+    //Example 1:
+    //
+    //Input: candies = [2,3,5,1,3], extraCandies = 3
+    //Output: [true,true,true,false,true]
+    //Explanation: If you give all extraCandies to:
+    //- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+    //- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+    //- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+    //- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+    //- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
     @Override
     public void execute() {
         int[] candies = {7, 2, 5, 6, 9, 9, 4, 5};
@@ -14,6 +34,8 @@ public class KidsWithTheGreatestNumberOfCandies implements AlgosActionDispatcher
         System.out.println(kidsWithCandies(candies, extraCandies));
     }
 
+
+    //Beats 100% of other submissions' runtime. Java.
     private List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
 
         List<Boolean> kidsWithGreatestCandies = new ArrayList<>();
