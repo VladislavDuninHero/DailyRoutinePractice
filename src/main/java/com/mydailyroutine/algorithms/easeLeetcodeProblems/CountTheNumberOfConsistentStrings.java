0361e27e75149be327578aa8d3a6f4a159dc7ef5
vladislavDunin <vladislavdunin.java@gmail.com>
2024-08-01 00:11:49 +0300

    Add countConsistentStrings class

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/CountTheNumberOfConsistentStrings.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/CountTheNumberOfConsistentStrings.java
@@ -2,8 +2,23 @@ package com.mydailyroutine.algorithms.easeLeetcodeProblems;
 
 import com.mydailyroutine.algorithms.AlgosActionDispatcher;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
+
+//Description:
+//You are given a string allowed consisting of distinct characters and an array of strings words. A string is consistent if all characters in the string appear in the string allowed.
+//
+//Return the number of consistent strings in the array words.
+//
+//
+//
+//Example 1:
+//
+//Input: allowed = "ab", words = ["ad","bd","aaab","baa","badab"]
+//Output: 2
+//Explanation: Strings "aaab" and "baa" are consistent since they only contain characters 'a' and 'b'.
 
 public class CountTheNumberOfConsistentStrings implements AlgosActionDispatcher {
 
@@ -21,7 +36,25 @@ public class CountTheNumberOfConsistentStrings implements AlgosActionDispatcher 
 
         int count = 0;
 
+        Set<Character> allowedList = new HashSet<>();
 
+        for (int i = 0; i < allowed.length(); i++) {
+            allowedList.add(allowed.charAt(i));
+        }
+
+        for (int i = 0; i < words.length; i++) {
+            boolean isConsistent = true;
+            for (int j = 0; j < words[i].length(); j++) {
+                if (!allowedList.contains(words[i].charAt(j))) {
+                    isConsistent = false;
+                    break;
+                }
+            }
+
+            if (isConsistent) {
+                count++;
+            }
+        }
 
         return count;
     }
