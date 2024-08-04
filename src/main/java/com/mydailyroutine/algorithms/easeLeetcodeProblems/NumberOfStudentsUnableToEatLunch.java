c4e7db556d7a0e5491885b684d5a83d4da727616
vladislavDunin <vladislavdunin.java@gmail.com>
2024-08-04 23:42:16 +0300

    Add NumberOfStudentsUnableToEatLunch class

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/NumberOfStudentsUnableToEatLunch.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/NumberOfStudentsUnableToEatLunch.java
@@ -6,6 +6,16 @@ public class NumberOfStudentsUnableToEatLunch implements AlgosActionDispatcher {
 
     @Override
     public void execute() {
+        int[] students = {1, 1, 0, 0};
+        int[] sandwiches = {0, 1, 0, 1};
 
+        int count = countStudents(students, sandwiches);
+
+        System.out.println(count);
+    }
+
+    private int countStudents(int[] students, int[] sandwiches) {
+
+        return 0;
     }
 }
