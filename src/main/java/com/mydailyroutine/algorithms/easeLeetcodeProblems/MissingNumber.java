5f6287c796e7909c1ed4c4d489bd68dfe58b3b7f
vladislavDunin <vladislavdunin.java@gmail.com>
2024-09-09 23:25:10 +0300

    Add MissingNumber class

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/MissingNumber.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/MissingNumber.java
@@ -1,4 +1,16 @@
 package com.mydailyroutine.algorithms.easeLeetcodeProblems;
 
-public class MissingNumber {
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class MissingNumber implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+
+    }
+
+    private int missingNumber(int[] nums) {
+
+        return 0;
+    }
 }
