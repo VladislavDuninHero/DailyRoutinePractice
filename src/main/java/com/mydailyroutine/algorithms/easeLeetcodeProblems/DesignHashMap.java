54301320a3c74fbb18fe917caba8c34d932be6fd
vladislavDunin <vladislavdunin.java@gmail.com>
2024-09-16 23:31:50 +0300

    Add class DesignHashMap

---
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/DesignHashMap.java
@@ -0,0 +1,12 @@
+package com.mydailyroutine.algorithms.easeLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class DesignHashMap implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+
+    }
+
+}
