efac32027d05668735f93ce3bceb801fb60285b1
vladislavDunin <vladislavdunin.java@gmail.com>
2024-09-17 22:21:03 +0300

    Add class DesignHashMap

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/DesignHashMap.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/DesignHashMap.java
@@ -2,11 +2,66 @@ package com.mydailyroutine.algorithms.easeLeetcodeProblems;
 
 import com.mydailyroutine.algorithms.AlgosActionDispatcher;
 
+import java.util.Arrays;
+
+
+//Description:
+//Design a HashMap without using any built-in hash table libraries.
+//
+//Implement the MyHashMap class:
+//
+//MyHashMap() initializes the object with an empty map.
+//void put(int key, int value) inserts a (key, value) pair into the HashMap. If the key already exists in the map, update the corresponding value.
+//int get(int key) returns the value to which the specified key is mapped, or -1 if this map contains no mapping for the key.
+//void remove(key) removes the key and its corresponding value if the map contains the mapping for the key.
+//
+//
+//Example 1:
+//
+//Input
+//["MyHashMap", "put", "put", "get", "get", "put", "get", "remove", "get"]
+//[[], [1, 1], [2, 2], [1], [3], [2, 1], [2], [2], [2]]
+//Output
+//[null, null, null, 1, -1, null, 1, null, -1]
+//
+//Explanation
+//MyHashMap myHashMap = new MyHashMap();
+//myHashMap.put(1, 1); // The map is now [[1,1]]
+//myHashMap.put(2, 2); // The map is now [[1,1], [2,2]]
+//myHashMap.get(1);    // return 1, The map is now [[1,1], [2,2]]
+//myHashMap.get(3);    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
+//myHashMap.put(2, 1); // The map is now [[1,1], [2,1]] (i.e., update the existing value)
+//myHashMap.get(2);    // return 1, The map is now [[1,1], [2,1]]
+//myHashMap.remove(2); // remove the mapping for 2, The map is now [[1,1]]
+//myHashMap.get(2);    // return -1 (i.e., not found), The map is now [[1,1]]
+
+
+//Runtime: 33% of other solutions.
+//Memory: 44% of other solutions.
 public class DesignHashMap implements AlgosActionDispatcher {
+
+    private int[] arrayMap;
 
     @Override
     public void execute() {
 
+    }
+
+    public DesignHashMap() {
+        arrayMap = new int[100001];
+        Arrays.fill(arrayMap, -1);
+    }
+
+    public void put(int key, int value) {
+        arrayMap[key] = value;
+    }
+
+    public int get(int key) {
+        return arrayMap[key];
+    }
+
+    public void remove(int key) {
+        arrayMap[key] = -1;
     }
 
 }
