964ff547d2d53e5258d1acdaed5b00b234837e62
vladislavDunin <vladislavdunin.java@gmail.com>
2024-09-26 22:14:53 +0300

    Add maxNumberOfBalloons class

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/MaximumNumberOfBalloons.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/MaximumNumberOfBalloons.java
@@ -3,7 +3,9 @@ package com.mydailyroutine.algorithms.easeLeetcodeProblems;
 import com.mydailyroutine.algorithms.AlgosActionDispatcher;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MaximumNumberOfBalloons implements AlgosActionDispatcher {
     @Override
@@ -18,32 +20,8 @@ public class MaximumNumberOfBalloons implements AlgosActionDispatcher {
     private int maxNumberOfBalloons(String text) {
         String balloon = "balloon";
 
-        StringBuilder builder = new StringBuilder();
+        Map<String, Integer> balloonsStatistic = new HashMap<>();
 
-        char[] letters = text.toCharArray();
-
-        List<String> balloons = new ArrayList<>();
-
-        for (int i = 0; i < letters.length; i++) {
-            for (int j = 0; j < balloon.length(); j++) {
-                if (balloon.charAt(j) == letters[i]) {
-                    builder.append(balloon.charAt(j));
-
-                    letters[i] = 0;
-
-                    break;
-                }
-            }
-
-            String potentialBalloon = builder.toString();
-
-            if (potentialBalloon.equals(balloon)) {
-                balloons.add(potentialBalloon);
-
-                builder.delete(0, builder.length());
-            }
-        }
-
-        return balloons.size();
+        return 0;
     }
 }
