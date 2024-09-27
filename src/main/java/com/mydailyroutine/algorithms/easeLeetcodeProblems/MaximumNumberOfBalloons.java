ab11624f34aba307da1107517281f097993643ca
vladislavDunin <vladislavdunin.java@gmail.com>
2024-09-27 23:05:32 +0300

    Add maxNumberOfBalloons class

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/MaximumNumberOfBalloons.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/MaximumNumberOfBalloons.java
@@ -20,6 +20,26 @@ public class MaximumNumberOfBalloons implements AlgosActionDispatcher {
     private int maxNumberOfBalloons(String text) {
         String balloon = "balloon";
 
+        String[] targetArray = text.split("");
+
+        Map<Character, Integer> frequency = new HashMap<>();
+
+        for (int i = 0; i < text.length(); i++) {
+            if (!frequency.containsKey(text.charAt(i))) {
+                frequency.put(text.charAt(i), 1);
+                continue;
+            }
+
+            frequency.put(text.charAt(i), frequency.get(text.charAt(i)) + 1);
+        }
+
+        List<String> listOfBalloons = new ArrayList<>();
+        StringBuilder builder = new StringBuilder();
+
+
+
+        System.out.println(listOfBalloons);
+
         return 0;
     }
 }
