c9395dabb03a7417b966a614dd823fb6a986d6f6
vladislavDunin <vladislavdunin.java@gmail.com>
2024-10-03 22:16:36 +0300

    Add KthDistinctStringInAnArray class

---
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/KthDistinctStringInAnArray.java
@@ -0,0 +1,15 @@
+package com.mydailyroutine.algorithms.easeLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class KthDistinctStringInAnArray implements AlgosActionDispatcher {
+    @Override
+    public void execute() {
+
+    }
+
+    private String kthDistinct(String[] arr, int k) {
+        return null;
+    }
+}
+
