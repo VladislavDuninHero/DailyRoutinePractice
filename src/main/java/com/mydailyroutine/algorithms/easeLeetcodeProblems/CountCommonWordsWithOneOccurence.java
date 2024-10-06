59d770b39db837543a2603417f2dceb6ec63fdbc
vladislavDunin <vladislavdunin.java@gmail.com>
2024-10-07 00:10:41 +0300

    Add new methods to CountCommonWordsWithOneOccurence class

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/CountCommonWordsWithOneOccurence.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/CountCommonWordsWithOneOccurence.java
@@ -4,6 +4,32 @@ import com.mydailyroutine.algorithms.AlgosActionDispatcher;
 
 import java.util.*;
 
+//Description:
+//Given two string arrays words1 and words2, return the number of strings that appear exactly once in each of the two arrays.
+//
+//
+//
+//Example 1:
+//
+//Input: words1 = ["leetcode","is","amazing","as","is"], words2 = ["amazing","leetcode","is"]
+//Output: 2
+//Explanation:
+//- "leetcode" appears exactly once in each of the two arrays. We count this string.
+//- "amazing" appears exactly once in each of the two arrays. We count this string.
+//- "is" appears in each of the two arrays, but there are 2 occurrences of it in words1. We do not count this string.
+//- "as" appears once in words1, but does not appear in words2. We do not count this string.
+//Thus, there are 2 strings that appear exactly once in each of the two arrays.
+//Example 2:
+//
+//Input: words1 = ["b","bb","bbb"], words2 = ["a","aa","aaa"]
+//Output: 0
+//Explanation: There are no strings that appear in each of the two arrays.
+//Example 3:
+//
+//Input: words1 = ["a","ab"], words2 = ["a","a","a","ab"]
+//Output: 1
+//Explanation: The only string that appears exactly once in each of the two arrays is "ab".
+
 public class CountCommonWordsWithOneOccurence implements AlgosActionDispatcher {
 
     @Override
@@ -12,8 +38,37 @@ public class CountCommonWordsWithOneOccurence implements AlgosActionDispatcher {
         String[] arrayOfWordsTwo = {"amazing", "leetcode", "is"};
 
         int countOfWords = countWords(arrayOfWordsOne, arrayOfWordsTwo);
+        int countOfWordsAlternative = countOfWordsAlternative(arrayOfWordsOne, arrayOfWordsTwo);
+        int countOfWordsWithTwoHashMaps = countOfWordsWithTwoHashMaps(arrayOfWordsOne, arrayOfWordsTwo);
 
         System.out.println(countOfWords);
+        System.out.println(countOfWordsAlternative);
+        System.out.println(countOfWordsWithTwoHashMaps);
+    }
+
+    //Runtime: beats 56%.
+    //Memory: beats 45%.
+    private int countOfWordsWithTwoHashMaps(String[] words1, String[] words2) {
+        Map<String, Integer> firstMap = new HashMap<>();
+        Map<String, Integer> secondMap = new HashMap<>();
+
+        for (String word : words1) {
+            firstMap.put(word, firstMap.getOrDefault(word, 0) + 1);
+        }
+
+        for (String word : words2) {
+            secondMap.put(word, secondMap.getOrDefault(word, 0) + 1);
+        }
+
+        int count = 0;
+
+        for (Map.Entry<String, Integer> entry : firstMap.entrySet()) {
+            if (entry.getValue() == 1 && secondMap.getOrDefault(entry.getKey(), 0) == 1) {
+                count++;
+            }
+        }
+
+        return count;
     }
 
     //Runtime: beats 5%.
@@ -56,23 +111,33 @@ public class CountCommonWordsWithOneOccurence implements AlgosActionDispatcher {
 
         int count = 0;
 
-        for (int i = 0; i < words1.length; i++) {
+        addWithoutDuplicates(noDuplicateWordsFirst, words1);
+        addWithoutDuplicates(noDuplicateWordsSecond, words2);
 
-            if (!noDuplicateWordsFirst.contains(words1[i])) {
-                noDuplicateWordsFirst.add(words1[i]);
-            }
-
-            count++;
-
-            if (count == 1) {
-
+        for (String word : noDuplicateWordsFirst) {
+            if (noDuplicateWordsSecond.contains(word)) {
+                count++;
             }
         }
 
-        for (int i = 0; i < words2.length; i++) {
-
-        }
-
-        return 0;
+        return count;
     }
+
+    private void addWithoutDuplicates(List<String> words, String[] array) {
+        for (int i = 0; i < array.length; i++) {
+            boolean isReplicated = true;
+            for (int j = 0; j < array.length; j++) {
+                if (array[i].equals(array[j]) && i != j) {
+                    isReplicated = true;
+                    break;
+                }
+                isReplicated = false;
+            }
+
+            if (!isReplicated) {
+                words.add(array[i]);
+            }
+        }
+    }
+
 }
