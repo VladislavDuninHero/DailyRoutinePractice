20e03cd06555e233817478fa16616b64053dcb16
vladislavDunin <vladislavdunin.java@gmail.com>
2024-10-21 23:57:56 +0300

    add NumberOfSeniorCitizens class

---
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/NumberOfSeniorCitizens.java
@@ -0,0 +1,14 @@
+package com.mydailyroutine.algorithms.easeLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class NumberOfSeniorCitizens implements AlgosActionDispatcher {
+    @Override
+    public void execute() {
+
+    }
+
+    private int countSeniors(String[] details) {
+        return 0;
+    }
+}
