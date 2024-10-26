eb10e9eb95c726adc864f8ae68176f091711b1e8
vladislavDunin <vladislavdunin.java@gmail.com>
2024-10-26 23:56:05 +0300

    add FindMaximumNumberOfStringPairs class

---
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/FindMaximumNumberOfStringPairs.java
@@ -0,0 +1,15 @@
+package com.mydailyroutine.algorithms.easeLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class FindMaximumNumberOfStringPairs implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+
+    }
+
+    private int maximumNumberOfStringPairs(String[] words) {
+        return 0;
+    }
+}
