d0ac2e7b3c1a9994c412976bd321bb935761d9e9
vladislavDunin <vladislavdunin.java@gmail.com>
2024-11-05 23:28:25 +0300

    add AntOnTheBoundary class

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/AntOnTheBoundary.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/AntOnTheBoundary.java
@@ -2,49 +2,54 @@ package com.mydailyroutine.algorithms.easeLeetcodeProblems;
 
 import com.mydailyroutine.algorithms.AlgosActionDispatcher;
 
+//Description:
+//An ant is on a boundary. It sometimes goes left and sometimes right.
+//
+//You are given an array of non-zero integers nums. The ant starts reading nums from the first element of it to its end. At each step, it moves according to the value of the current element:
+//
+//If nums[i] < 0, it moves left by -nums[i] units.
+//If nums[i] > 0, it moves right by nums[i] units.
+//Return the number of times the ant returns to the boundary.
+//
+//Notes:
+//
+//There is an infinite space on both sides of the boundary.
+//We check whether the ant is on the boundary only after it has moved |nums[i]| units. In other words, if the ant crosses the boundary during its movement, it does not count.
+//
+//
+//Example 1:
+//
+//Input: nums = [2,3,-5]
+//Output: 1
+//Explanation: After the first step, the ant is 2 steps to the right of the boundary.
+//After the second step, the ant is 5 steps to the right of the boundary.
+//After the third step, the ant is on the boundary.
+//So the answer is 1.
+
 public class AntOnTheBoundary implements AlgosActionDispatcher {
 
     @Override
     public void execute() {
-        int[] nums = {2, 3, -5};
+        int[] nums = {3, 2, -3, -4};
 
         int boundaryCount = returnToBoundaryCount(nums);
 
         System.out.println(boundaryCount);
     }
 
+    //Runtime: beats 100% of other solutions.
+    //Memory: beats 33% of other solutions.
     private int returnToBoundaryCount(int[] nums) {
 
         int boundaryCount = 0;
+        int sum = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                int stepIndex = i + nums[i] + 1;
-
-                if (stepIndex > nums.length) {
-                    while (stepIndex > nums.length) {
-
-                    }
-                }
-
-                if (nums[stepIndex + 1] > nums.length || nums[stepIndex - 1] < 0) {
-                    boundaryCount++;
-                }
-
-                System.out.println(stepIndex);
-            }
-
-            if (nums[i] < 0) {
-                int stepIndex = i - nums[i];
-
-                if (nums[stepIndex - 1] < nums.length || nums[stepIndex + 1] > 0) {
-                    boundaryCount++;
-                }
-
-                System.out.println(stepIndex);
+        for (final int num : nums) {
+            if ((sum += num) == 0) {
+                boundaryCount++;
             }
         }
 
-        return 0;
+        return boundaryCount;
     }
 }
