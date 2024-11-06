fa1924755504ed3aa121adb42bb607ceb135a66a
vladislavDunin <vladislavdunin.java@gmail.com>
2024-11-06 23:35:34 +0300

    add DistributeElementsIntoTwoArraysI class

---
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/DistributeElementsIntoTwoArraysI.java
@@ -0,0 +1,16 @@
+package com.mydailyroutine.algorithms.easeLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class DistributeElementsIntoTwoArraysI implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+
+    }
+
+    private int[] resultArray(int[] nums) {
+
+        return null;
+    }
+}
