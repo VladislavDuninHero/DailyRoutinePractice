05e951b2b82381d6dbbfa32119c574c78276240d
vladislavDunin <vladislavdunin.java@gmail.com>
2024-11-08 23:55:51 +0300

    add SpecialArrayI class

---
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/SpecialArrayI.java
@@ -0,0 +1,15 @@
+package com.mydailyroutine.algorithms.easeLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class SpecialArrayI implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+
+    }
+
+    private boolean isArraySpecial(int[] nums) {
+        return true;
+    }
+}
