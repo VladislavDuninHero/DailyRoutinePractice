2e9fafd635cdbbdbe932fb6f053edb86e663c87d
vladislavDunin <vladislavdunin.java@gmail.com>
2024-11-12 20:51:28 +0300

    add FinalArrayStateAfterKMultiplicationOperationsI class

--- src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/FinalArrayStateAfterKMultiplicationOperationsI.java
+++ src/main/java/com/mydailyroutine/algorithms/easeLeetcodeProblems/FinalArrayStateAfterKMultiplicationOperationsI.java
@@ -4,6 +4,32 @@ import com.mydailyroutine.algorithms.AlgosActionDispatcher;
 
 import java.util.Arrays;
 
+//Description:
+//You are given an integer array nums, an integer k, and an integer multiplier.
+//
+//You need to perform k operations on nums. In each operation:
+//
+//Find the minimum value x in nums. If there are multiple occurrences of the minimum value, select the one that appears first.
+//Replace the selected minimum value x with x * multiplier.
+//Return an integer array denoting the final state of nums after performing all k operations.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [2,1,3,5,6], k = 5, multiplier = 2
+//
+//Output: [8,4,6,5,6]
+//
+//Explanation:
+//
+//Operation	Result
+//After operation 1	[2, 2, 3, 5, 6]
+//After operation 2	[4, 2, 3, 5, 6]
+//After operation 3	[4, 4, 3, 5, 6]
+//After operation 4	[4, 4, 6, 5, 6]
+//After operation 5	[8, 4, 6, 5, 6]
+
 public class FinalArrayStateAfterKMultiplicationOperationsI implements AlgosActionDispatcher {
 
     @Override
@@ -17,22 +43,23 @@ public class FinalArrayStateAfterKMultiplicationOperationsI implements AlgosActi
         System.out.println(Arrays.toString(finalState));
     }
 
+    //Runtime: beats 100% of other solutions.
+    //Memory: beats 85% of other solutions.
+    //Time complexity: O(k * n).
     private int[] getFinalState(int[] nums, int k, int multiplier) {
 
-        int[] copy = Arrays.copyOf(nums, nums.length);
-        Arrays.sort(copy);
-        System.out.println(Arrays.toString(copy));
-
-        for (int i = 0, copyPointer = 0; i < k; i++, copyPointer++) {
-            int startIndex = copy[copyPointer];
-
-            for (int j = 0; j < nums.length; j++) {
-                if (startIndex == nums[j]) {
-                    nums[j] = nums[j] * multiplier;
-                    break;
+        while (k > 0) {
+            int min = Integer.MAX_VALUE;
+            int index = 0;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] < min) {
+                    min = nums[i];
+                    index = i;
                 }
             }
 
+            nums[index] = nums[index] * multiplier;
+            k--;
         }
 
         return nums;
