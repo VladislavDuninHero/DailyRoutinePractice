122c5027540fb7f49ddc189723d0c1b6ca4ac75e
vladislavDunin <vladislavdunin.java@gmail.com>
2024-12-03 00:17:15 +0300

    Add SubdomainVisitCount class

---
+++ src/main/java/com/mydailyroutine/algorithms/mediumLeetcodeProblems/SubdomainVisitCount.java
@@ -0,0 +1,10 @@
+package com.mydailyroutine.algorithms.mediumLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class SubdomainVisitCount implements AlgosActionDispatcher {
+    @Override
+    public void execute() {
+
+    }
+}
