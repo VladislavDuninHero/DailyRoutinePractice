2f60ab4d9cecf2d811728c38f587f7c5e480e8da
vladislavDunin <vladislavdunin.java@gmail.com>
2024-12-06 00:05:39 +0300

    Add FindPlayersWithZeroOrOneLosses class

---
+++ src/main/java/com/mydailyroutine/algorithms/mediumLeetcodeProblems/FindPlayersWithZeroOrOneLosses.java
@@ -0,0 +1,18 @@
+package com.mydailyroutine.algorithms.mediumLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+import java.util.List;
+
+public class FindPlayersWithZeroOrOneLosses implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+
+    }
+
+    private List<List<Integer>> findWinners(int[][] matches) {
+        return null;
+    }
+
+}
