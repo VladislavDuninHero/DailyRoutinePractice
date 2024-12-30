b251004941568386a195f6d2e8587f4dc5461276
vladislavDunin <vladislavdunin.java@gmail.com>
2024-12-31 01:42:57 +0300

    Add LongestStringChain class

---
+++ src/main/java/com/mydailyroutine/algorithms/mediumLeetcodeProblems/LongestStringChain.java
@@ -0,0 +1,15 @@
+package com.mydailyroutine.algorithms.mediumLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class LongestStringChain implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+
+    }
+
+    private int longestStrChain(String[] words) {
+        return 0;
+    }
+}
