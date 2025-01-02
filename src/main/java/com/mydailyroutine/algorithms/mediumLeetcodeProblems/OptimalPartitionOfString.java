123570f6bfaa96b4dcb0cde8cf9fc0ab30c8759c
vladislavDunin <vladislavdunin.java@gmail.com>
2025-01-02 22:50:36 +0300

    Add OptimalPartitionOfString class

---
+++ src/main/java/com/mydailyroutine/algorithms/mediumLeetcodeProblems/OptimalPartitionOfString.java
@@ -0,0 +1,17 @@
+package com.mydailyroutine.algorithms.mediumLeetcodeProblems;
+
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class OptimalPartitionOfString implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+
+    }
+
+    private int partitionString(String s) {
+        return 0;
+    }
+
+}
