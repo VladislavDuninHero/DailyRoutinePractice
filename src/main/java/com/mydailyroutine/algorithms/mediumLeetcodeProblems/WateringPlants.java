215862b13993d88441e7025d039f74a7990e8a27
vladislavDunin <vladislavdunin.java@gmail.com>
2025-01-05 17:20:35 +0300

    Add WateringPlants class

---
+++ src/main/java/com/mydailyroutine/algorithms/mediumLeetcodeProblems/WateringPlants.java
@@ -0,0 +1,15 @@
+package com.mydailyroutine.algorithms.mediumLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class WateringPlants implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+
+    }
+
+    private int wateringPlants(int[] plants, int capacity) {
+        return 0;
+    }
+}
