3c3154d672596f038ce82c18d37ae78833722d84
vladislavDunin <vladislavdunin.java@gmail.com>
2025-02-25 21:13:05 +0300

    Add DesignBrowserHistory and MainBrowserHistory implementations

--- src/main/java/com/mydailyroutine/Main.java
+++ src/main/java/com/mydailyroutine/Main.java
@@ -1,13 +1,25 @@
 package com.mydailyroutine;
 
 import com.mydailyroutine.algorithms.binaryTree.BinaryTree;
+import com.mydailyroutine.algorithms.mediumLeetcodeProblems.browser.MainBrowserHistory;
 
 public class Main {
     public static void main(String[] args) {
         BinaryTree root =
                 new BinaryTree(20, new BinaryTree(7, new BinaryTree(4, null, new BinaryTree(6)), new BinaryTree(9)),
                         new BinaryTree(35,
-                                new BinaryTree(31, new BinaryTree(28), null),
-                                new BinaryTree(40, new BinaryTree(38), new BinaryTree(52))));
+                                new BinaryTree(31,
+                                        new BinaryTree(28),
+                                        null
+                                ),
+                                new BinaryTree(40,
+                                        new BinaryTree(38),
+                                        new BinaryTree(52)
+                                )
+                        )
+                );
+
+        MainBrowserHistory mainBrowserHistory = new MainBrowserHistory();
+        mainBrowserHistory.execute();
     }
 }
---
+++ src/main/java/com/mydailyroutine/algorithms/mediumLeetcodeProblems/browser/DesignBrowserHistory.java
@@ -0,0 +1,48 @@
+package com.mydailyroutine.algorithms.mediumLeetcodeProblems.browser;
+
+
+import lombok.Getter;
+
+import java.util.ArrayList;
+
+@Getter
+public class DesignBrowserHistory {
+
+    private final String homePage;
+    private final ArrayList<String> history;
+    private String currentPage;
+
+    public DesignBrowserHistory(String homepage) {
+        this.homePage = homepage;
+        this.history = new ArrayList<>();
+        this.history.add(homePage);
+        this.currentPage = homePage;
+    }
+
+    public void visit(String url) {
+        this.history.add(url);
+        this.currentPage = url;
+    }
+
+    public String back(int steps) {
+        if (history.size() == 1) {
+            return homePage;
+        }
+
+        int index = history.indexOf(currentPage) - steps;
+        currentPage = index < 0 ? history.get(index) : homePage;
+
+        return currentPage;
+    }
+
+    public String forward(int steps) {
+        if (history.size() == 1) {
+            return homePage;
+        }
+
+        int index = history.indexOf(currentPage) + steps;
+        currentPage = index > 0 ? history.get(history.size() - 1) : homePage;
+
+        return currentPage;
+    }
+}
