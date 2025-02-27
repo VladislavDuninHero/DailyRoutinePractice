b845577f4928091e5329b4bd3b4662276da74046
vladislavDunin <vladislavdunin.java@gmail.com>
2025-02-27 22:07:59 +0300

    Add KeysAndRooms class

---
+++ src/main/java/com/mydailyroutine/algorithms/mediumLeetcodeProblems/KeysAndRooms.java
@@ -0,0 +1,12 @@
+package com.mydailyroutine.algorithms.mediumLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class KeysAndRooms implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+
+    }
+
+}
