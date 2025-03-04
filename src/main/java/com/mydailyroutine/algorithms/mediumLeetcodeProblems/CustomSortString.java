801de1338956c5f2417e5d06ade495dc66b1786a
vladislavDunin <vladislavdunin.java@gmail.com>
2025-03-04 22:31:21 +0300

    Update CustomSortString class

--- src/main/java/com/mydailyroutine/algorithms/mediumLeetcodeProblems/CustomSortString.java
+++ src/main/java/com/mydailyroutine/algorithms/mediumLeetcodeProblems/CustomSortString.java
@@ -2,6 +2,9 @@ package com.mydailyroutine.algorithms.mediumLeetcodeProblems;
 
 import com.mydailyroutine.algorithms.AlgosActionDispatcher;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CustomSortString implements AlgosActionDispatcher {
 
     @Override
@@ -15,6 +18,20 @@ public class CustomSortString implements AlgosActionDispatcher {
     }
 
     private String customSortString(String order, String s) {
+
+        StringBuilder builder = new StringBuilder();
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            sMap.put(s.charAt(i), i);
+        }
+
+        Map<Character, Integer> orderMap = new HashMap<>();
+        for (int i = 0; i < order.length(); i++) {
+            orderMap.put(order.charAt(i), i);
+        }
+
+
         return null;
     }
 }
