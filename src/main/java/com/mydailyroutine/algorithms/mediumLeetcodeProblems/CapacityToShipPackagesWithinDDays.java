1c29c7bc651dc810bac065eea53cda2d17c66756
vladislavDunin <vladislavdunin.java@gmail.com>
2025-03-04 22:37:55 +0300

    Delete CapacityToShipPackagesWithinDDays class

---
+++ src/main/java/com/mydailyroutine/algorithms/mediumLeetcodeProblems/CapacityToShipPackagesWithinDDays.java
@@ -0,0 +1,11 @@
+package com.mydailyroutine.algorithms.mediumLeetcodeProblems;
+
+import com.mydailyroutine.algorithms.AlgosActionDispatcher;
+
+public class CapacityToShipPackagesWithinDDays implements AlgosActionDispatcher {
+
+    @Override
+    public void execute() {
+
+    }
+}
